aa77dc2691610c985d458fe9ce9134010725cd59
MatteoSala97 <matteo111997@gmail.com>
2024-01-11 14:04:53 +0100

    feat: first snack completed

---
+++ blocco-1/jsnack3/main.js
@@ -0,0 +1,7 @@
+//Creating loop 10 times
+for (let i = 0; i < 10; i++) {
+    //  prompt the user to type a number
+    let numberRequest = prompt("Please enter a random number:")
+    //prints the result in console
+    console.log(`User entered: ${numberRequest}`)
+  }
