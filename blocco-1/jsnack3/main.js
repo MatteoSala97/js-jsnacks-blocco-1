6ea50e54ef8f87c280a64d187fee01d644ecad66
MatteoSala97 <matteo111997@gmail.com>
2024-01-11 14:39:17 +0100

    minor fixes

--- blocco-1/jsnack3/main.js
+++ blocco-1/jsnack3/main.js
@@ -1,18 +1,12 @@
-const array = []
 let sum = 0
-//Creating loop 10 times
-for (let i = 0; i < 3; i++) {
+
+for (let i = 0; i < 10; i++) {
 
   //  prompts the user to type a number
   let numberRequest = prompt("Please enter a random number:")
-
-  // converts the input into a number
-  array.push(Number(numberRequest))
-  console.log(`User entered: ${numberRequest}`)
-}
   
-for (let i = 0; i < array.length; i++){
-  sum += array[i]
+  sum += Number(numberRequest)
+  console.log(`User submitted: ${numberRequest}`)
 }
-console.log(array)
-console.log(`The sum of all user's submitted numbers is: ${sum}`)
+
+console.log(`Sum of all numbers: ${sum}`)
