41d32c919aa03d7ba80288b52f6e1feec20425d2
MatteoSala97 <matteo111997@gmail.com>
2024-01-11 16:42:53 +0100

    feat: blocco-2 jsnack2 randomizes on click - completed

--- blocco-2/jsnack2/main.js
+++ blocco-2/jsnack2/main.js
@@ -1,18 +1,19 @@
 //declaring array of names
 const namesArray = ["Diego", "Mattia", "Samuele", "Luca", "Marco", "Alessandro", "Francesco", "Matteo"]
-
 //declaring array of surnames
 const surnamesArray = ["Rossi", "Bianchi", "Verdi", "Ferrari", "Gallo", "Costa", "Russo ", "Marino " ]
+// declaring const for html ids
+const nameHtml = document.getElementById("name")
+const surnameHtml = document.getElementById("surname")
+const btnHtml = document.getElementById("btn")
+const resultHtml = document.getElementById("resultNameSurname")
 
 //creating a function that picks a random name and surname from the arrays
-const randomName = namesArray[Math.floor(Math.random() * namesArray.length)]
-const randomSurname = surnamesArray[Math.floor(Math.random() * surnamesArray.length)]
+btnHtml.addEventListener("click", function(){
+    const randomName = namesArray[Math.floor(Math.random() * namesArray.length)]
+    const randomSurname = surnamesArray[Math.floor(Math.random() * surnamesArray.length)]
 
-//prints the randomized names and surnames
-console.log(randomName)
-console.log(randomSurname)
-
-document.getElementById("name").innerHTML = randomName
-document.getElementById("surname").innerHTML = randomSurname
-
-document.getElementById("resultNameSurname").innerHTML = randomName + " " + randomSurname
+    nameHtml.innerHTML = randomName
+    surnameHtml.innerHTML = randomSurname
+    resultHtml.innerHTML = randomName + " " + randomSurname
+})
