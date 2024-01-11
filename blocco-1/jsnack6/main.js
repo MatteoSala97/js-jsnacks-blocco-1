ae1e522227c7367d7aca93a7fc2ee1eedef7384a
MatteoSala97 <matteo111997@gmail.com>
2024-01-11 15:23:15 +0100

    fixed blocco 1 jsnack 6

--- blocco-1/jsnack6/main.js
+++ blocco-1/jsnack6/main.js
@@ -1,5 +1,5 @@
 // prompts the user to sumbit a number
-let numberRequest = prompt("Submit a number")
+let numberRequest = prompt("Submit a number:")
 //prints the number submitted by the user into console
 console.log(numberRequest)
 
@@ -8,4 +8,17 @@ function cubeOf(numberRequest){
     console.log(cube)
 }
 
+
+// Ho creato 2 funzioni che adempiano alla richiesta dell'esercizio secondo i due modi in cui ho interpretato la consegna.
+
+
 cubeOf(numberRequest)
+
+// prompts the user to input a number
+let numberRequestSecondary = Number(prompt("Submit a number:"))
+
+// prints the cube number of all the numbers up until the number given by the user
+for (let i = 1; i <= numberRequestSecondary; i++) {
+    let cubo = i * i * i
+    console.log("Cubic of", i, "is:", cubo)
+}
