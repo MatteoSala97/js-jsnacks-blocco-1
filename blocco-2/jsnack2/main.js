ceba1a137ea6f2b02be7bf38df1665c0a8c3c35e
MatteoSala97 <matteo111997@gmail.com>
2024-01-11 15:54:22 +0100

    blocco 2 - snack 2 html and css config + js const

---
+++ blocco-2/jsnack2/main.js
@@ -0,0 +1,5 @@
+//declaring array of names
+const names = ["Diego", "Mattia", "Samuele", "Luca", "Marco", "Alessandro", "Francesco", "Matteo"]
+
+//declaring array of surnames
+const surnames = ["Rossi", "Bianchi", "Verdi", "Ferrari", "Gallo", "Costa", "Russo ", "Marino " ]
