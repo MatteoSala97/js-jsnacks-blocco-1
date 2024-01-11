c3437d3e8408d8d32eda912c2227de3c1c859f4a
MatteoSala97 <matteo111997@gmail.com>
2024-01-11 14:39:07 +0100

    fixed blocco-1 jsnack3 (missing sum of all numbers)

--- blocco-1/jsnack3/main.js
+++ blocco-1/jsnack3/main.js
@@ -1,7 +1,18 @@
+const array = []
+let sum = 0
 //Creating loop 10 times
-for (let i = 0; i < 10; i++) {
-    //  prompt the user to type a number
-    let numberRequest = prompt("Please enter a random number:")
-    //prints the result in console
-    console.log(`User entered: ${numberRequest}`)
-  }
+for (let i = 0; i < 3; i++) {
+
+  //  prompts the user to type a number
+  let numberRequest = prompt("Please enter a random number:")
+
+  // converts the input into a number
+  array.push(Number(numberRequest))
+  console.log(`User entered: ${numberRequest}`)
+}
+  
+for (let i = 0; i < array.length; i++){
+  sum += array[i]
+}
+console.log(array)
+console.log(`The sum of all user's submitted numbers is: ${sum}`)
