c6044660cf3d8ae82945d3eb683acc5cdf1418c0
MatteoSala97 <matteo111997@gmail.com>
2024-01-11 14:41:06 +0100

    added comments on jsnack3

--- blocco-1/jsnack3/main.js
+++ blocco-1/jsnack3/main.js
@@ -5,8 +5,9 @@ for (let i = 0; i < 10; i++) {
   //  prompts the user to type a number
   let numberRequest = prompt("Please enter a random number:")
   
+  // sums the numbers up
   sum += Number(numberRequest)
   console.log(`User submitted: ${numberRequest}`)
 }
-
+// prints sum to console
 console.log(`Sum of all numbers: ${sum}`)
